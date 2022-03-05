ad8982a511c3a80cafde8796ea0c6ec54a3591dd
satochanpe <satoshi.muko@gmail.com>
2022-03-05 14:29:09 +0000

    管理者機能の追加

--- app/User.php
+++ app/User.php
@@ -10,30 +10,29 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    public static $rules = array(
+        'name' => 'required',
+        'email' => 'required',
+        'password' => 'required',
+    );
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
+
+    public function isAdmin(){
+        return $this->role == 1;
+    }
 }
